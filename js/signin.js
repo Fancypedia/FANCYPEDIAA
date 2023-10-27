a3afe2e6e21d9ce13aaac1f4c55752738f1f6cbf
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-10-27 22:33:34 +0700

    Update (API) : Memperbaiki javascript dan API signin signup

--- js/signin.js
+++ js/signin.js
@@ -1,12 +1,10 @@
-
-
 document.addEventListener("DOMContentLoaded", function() {
     const loginForm = document.getElementById("loginForm");
     const message = document.getElementById("message");
 
     loginForm.addEventListener("submit", function(event) {
         event.preventDefault();
-        
+
         const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
 
@@ -30,52 +28,41 @@ document.addEventListener("DOMContentLoaded", function() {
                 message.style.color = "green";
                 console.log(token);
 
-                window.location.href = "../choices.html";
-                
-                
-                // After successful login, fetch user data and filter it
-                fetch("https://asia-southeast2-testlogin-366704.cloudfunctions.net/createcontent-3")
-                .then(response => response.json())
-                .then(userList => {
-                    const user = userList.find(user => user.username === username);
+                // Determine the role of the user here
+                const userRole = data.role;
 
+                if (userRole === "user") {
+                    // User has the "user" role, use the user API
+                    fetch("https://asia-southeast2-testlogin-366704.cloudfunctions.net/createcontent-3")
+                    .then(response => response.json())
+                    .then(userList => {
+                        const user = userList.find(user => user.username === username);
 
-                    if (user) {
-                        
-                        // User found, navigate to the dashboard
-                        window.location.href = "../choices.html";
-                    } else {
-                        // User not found
-                        message.textContent = "User not found in the database.";
-                        message.style.color = "red";
-                    }
-                })
-                .catch(error => {
-                    console.error("Error fetching user data:", error);
-                });
+                        if (user) {
+                            // User found, navigate to the dashboard for users
+                            window.location.href = "../choices.html";
+                        } else {
+                            // User not found
+                            message.textContent = "User not found in the database.";
+                            message.style.color = "red";
+                        }
+                    })
+                    .catch(error => {
+                        console.error("Error fetching user data:", error);
+                    });
+                } else {
+                    // User doesn't have a specific role or it's not "user"
+                    message.textContent = "You don't have the necessary role to access this page.";
+                    message.style.color = "red";
+                }
             } else {
-                fetch("https://asia-southeast2-testlogin-366704.cloudfunctions.net/createcontent-3")
-                .then(response => response.json())
-                .then(userList => {
-                    const user = userList.find(user => user.username === username);
-
-
-                    if (user) {
-                        // User found, navigate to the dashboard
-                        window.location.href = "../user/user.html";
-                    } else {
-                        // User not found
-                        message.textContent = "User not found in the database.";
-                        message.style.color = "red";
-                    }
-                })
-                .catch(error => {
-                    console.error("Error fetching user data:", error);
-                });
+                // Handle the case when authentication fails
+                message.textContent = "Authentication failed.";
+                message.style.color = "red";
             }
         })
         .catch(error => {
             console.error("Error:", error);
-        });
-    });
+        });
+    });
 });
