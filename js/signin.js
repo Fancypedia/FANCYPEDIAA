89393ec1d345f0486ac85cc4112235c260af9db7
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-10-28 15:25:42 +0700

    Add (Popup Login Admin) : Popup button cms atau admin dashboard

--- js/signin.js
+++ js/signin.js
@@ -1,3 +1,5 @@
+import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js";
+
 document.addEventListener("DOMContentLoaded", function() {
     const loginForm = document.getElementById("loginForm");
     const message = document.getElementById("message");
@@ -22,14 +24,19 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.text()) // Read response as text
         .then(data => {
             if (data === "Login successful") {
+                const token = data.token;
                 message.textContent = "Login successful";
+                setCookieWithExpireHour("token",token,2);
                 message.style.color = "green";
                 console.log("Login successful");
+                console.log("Token:", token);
                 window.location.href = "../user/user.html";
+
             } else {
                 // Handle the case when authentication fails
                 message.textContent = "Authentication failed.";
                 message.style.color = "red";
+                console.log("Login gagal");
             }
         })
         .catch(error => {
--- js/signinadmin.js
+++ js/signinadmin.js
@@ -1,3 +1,5 @@
+import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js";
+
 document.addEventListener("DOMContentLoaded", function() {
     const loginFormAdmin = document.getElementById("loginFormAdmin");
     const message = document.getElementById("message");
@@ -9,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const password = document.getElementById("password").value;
 
         // Send a POST request to your authentication API
-        fetch("https://asia-southeast2-testlogin-366704.cloudfunctions.net/function-16", {
+        fetch("https://asia-southeast2-testlogin-366704.cloudfunctions.net/admin", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -24,11 +26,34 @@ document.addEventListener("DOMContentLoaded", function() {
             if (data.status === true) {
                 const token = data.token;
                 const welcomeMessage = data.message;
+                setCookieWithExpireHour("token",token,2);
                 message.textContent = welcomeMessage;
                 message.style.color = "green";
+                // window.location.href = "../choices.html";
                 console.log(token);
                 console.log(welcomeMessage);
-                window.location.href = "../choices.html";
+                
+
+                // Menampilkan popup untuk memilih CMS atau adminDashboard
+                const dashboardSelectionPopup = document.getElementById("dashboardSelectionPopup");
+                dashboardSelectionPopup.style.display = "block";
+
+                // Menangani pemilihan dashboard
+                const cmsDashboardBtn = document.getElementById("cmsDashboardBtn");
+                const adminDashboardBtn = document.getElementById("adminDashboardBtn");
+
+                cmsDashboardBtn.addEventListener("click", function() {
+                    // Redirect ke halaman CMS dashboard
+                    window.location.href = "https://fancypedia.github.io/cms/";
+                });
+
+                adminDashboardBtn.addEventListener("click", function() {
+                    // Redirect ke halaman Admin dashboard
+                    window.location.href = "https://fancypedia.github.io/adminDashboard/";
+                });
+                
+
+                
             } else {
                 // Handle the case when authentication fails
                 message.textContent = "Authentication failed.";
