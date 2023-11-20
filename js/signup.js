432718c34544ee3205c37ec5cbed5c69d216d998
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-11-20 20:25:36 +0700

    coba signup

--- js/signup.js
+++ js/signup.js
@@ -95,15 +95,15 @@
 
 
 import { postBiasa } from "./api.js";
-import { setInner,getValue } from "./element.js";
+import { setInner, getValue } from "./element.js";
 
-export default function RegistrasiUser(){
+export default function RegistrasiUser() {
     let target_url = "https://asia-southeast2-annular-hexagon-401501.cloudfunctions.net/signup-1fancy";
     let datainjson = {
         "username": getValue("usernamesignup"),
         "password": getValue("passwordsignup")
-    }
-    postBiasa(target_url,datainjson,responseData);
+    };
+    postBiasa(target_url, datainjson, responseData);
 }
 
 function responseData(result) {
@@ -118,3 +118,5 @@ function responseData(result) {
         alert("Berhasil Registrasi");
     }
 }
+
+
