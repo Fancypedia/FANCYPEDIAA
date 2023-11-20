8ba7d2096407e3aeb85f7380c75c5e11b977325d
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-11-20 13:14:22 +0700

    coba

--- js/checkCookie.js
+++ js/checkCookie.js
@@ -7,6 +7,6 @@ export default function checkCookie() {
 	alert("Welcome " + username);
 	} else {
 		alert("Biasakan Login Dulu Ya Bang!")
-		window.location.href="../theme/signin/";
+		window.location.href="../theme/signin/index.html";
 	}
 }
