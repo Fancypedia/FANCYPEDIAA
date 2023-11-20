b00c0df430dafffdd6a9de4826a5c2c7d3214deb
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-11-20 13:11:56 +0700

    [update] check Cookies

--- js/checkCookie.js
+++ js/checkCookie.js
@@ -1,12 +1,12 @@
-import { getCookie} from "./cookies.js";
+import { getCookie } from "./cookies.js";
 
 export default function checkCookie() {
     let user = getCookie("token");
     let username = getCookie("username");
-	if (user != "") {
+	if (user != "" || user == null || user == undefined) {
 	alert("Welcome " + username);
 	} else {
-		alert("Biasakan login sebelum melakukan sesuatu");
-		window.location.href="/theme/signin.html";
+		alert("Biasakan Login Dulu Ya Bang!")
+		window.location.href="../theme/signin/";
 	}
 }
