be6b7b961c3da4836e03281cb0645da804e4975c
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-11-29 00:56:34 +0700

    add : animation loading indicator

--- js/gudangAPI.js
+++ js/gudangAPI.js
@@ -1,3 +1,4 @@
 export let APIRegistrasi = "https://asia-southeast2-annular-hexagon-401501.cloudfunctions.net/signup-1fancy";
 export let APILogin = "https://asia-southeast2-annular-hexagon-401501.cloudfunctions.net/signin-1fancy";
+// export let APIAuthorization = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-Authorization";
 
